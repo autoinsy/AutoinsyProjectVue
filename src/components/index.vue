2372ae734404668dab6a63fe5d5d7c746bd38079
Administrator <dawei685638@hotmail.com>
2018-10-29 10:06:22 +0800

    修改js

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,10 +1,26 @@
 <template>
-
+  <div class='' id="show">
+    <top-nav></top-nav>
+    <Nav></Nav>
+    <footer></footer>
+  </div>
 </template>
 
 <script>
+    import footer from '@/components/footer'
+    import Nav from '@/components/Nav'
+    import TopNav from '@/components/TopNav'
+
     export default {
-        name: "index"
+      name: "index",
+      components: {
+        footer: footer,
+        Nav: Nav,
+        TopNav: TopNav,
+      },
+
+      data() {
+      },
     }
 </script>
 
